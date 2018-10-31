a21448e3d6c47d052d0d93373ef01f7854d0272e
hubukinokaze <yudai.j.kawa@gmail.com>
2018-10-30 20:56:07 -0700

    +created basic pages +basic routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,31 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LandingComponent } from './components/landing/landing.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+import { LoginComponent } from './components/login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+	{
+		path: 'landing',
+		component: LandingComponent,
+		data: { title: 'Project Million' }
+	},{
+		path: 'login',
+		component: LoginComponent,
+		data: { title: 'Project Million' }
+	},{
+		path: 'dashboard',
+		component: LoginComponent,
+		data: { title: 'Project Million' }
+	},{
+		path: '',
+		redirectTo: '/landing',
+		pathMatch: 'full'
+	},{
+		path: '**',
+		component: PageNotFoundComponent
+	}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { LandingComponent } from './components/landing/landing.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    LandingComponent,
+    DashboardComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
