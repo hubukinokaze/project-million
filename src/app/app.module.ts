ceba9d1a64a3a150a3a19c7e58a1f39a84688f07
hubukinokaze <yudai.j.kawa@gmail.com>
2018-11-08 00:36:14 -0800

    +updated landing/login styling +added stock images

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
 import { LandingComponent } from './components/landing/landing.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+import { AuthGuard } from './auth.guard';
+
+import {
+  MatButtonModule,
+  MatMenuModule,
+  MatToolbarModule,
+  MatIconModule,
+  MatCardModule,
+  MatFormFieldModule,
+  MatInputModule
+} from '@angular/material';
 
 @NgModule({
   declarations: [
@@ -18,9 +32,20 @@ import { PageNotFoundComponent } from './components/page-not-found/page-not-foun
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+
+    MatButtonModule,
+    MatMenuModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule
   ],
-  providers: [],
+  providers: [AuthGuard],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { Validators, FormBuilder, FormGroup, FormControl } from '@angular/forms';
+import { AuthService } from '../../services/auth/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -7,15 +9,47 @@ import { Router } from '@angular/router';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+  public loginForm: FormGroup;
+  public toggleErrorMsg: boolean;
+  public errorMsg: string;
+  public invalidEmailMsg: string;
+  public invalidPWMsg: string;
 
   constructor(
-  	private router: Router
-  	) { }
+    private router: Router,
+    private formBuilder: FormBuilder,
+    private auth: AuthService
+    ) { }
 
   ngOnInit() {
+    this.loginForm = this.formBuilder.group({
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', Validators.required)
+    });
+
+    this.setupVariables();
   }
 
-  public goToDashboard(event) {
-  	this.router.navigate(['/dashboard']);
+  private setupVariables() {
+    this.errorMsg = "Incorrect email/password";
+    this.invalidEmailMsg = "Not a valid email";
+    this.invalidPWMsg = "Password cannot be empty";
+  }
+
+  public login(e) {
+    if (this.loginForm.valid) {
+      if (this.auth.authLogin(this.loginForm.value.email, this.loginForm.value.password)) {
+        this.router.navigateByUrl('/dashboard');
+      } else {
+        this.toggleErrorMsg = true;
+        setTimeout(() => {
+          this.toggleErrorMsg = false;
+        }, 3000);
+      }
+    }
+  }
+
+  public backToLanding() {
+    this.router.navigateByUrl('/landing');
   }
 }
