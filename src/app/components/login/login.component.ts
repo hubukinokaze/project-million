f46f3fbf6fba5ba11f8de55e81a05e68d5fa0a9d
hubukinokaze <yudai.j.kawa@gmail.com>
2018-11-23 12:44:24 -0800

    +Added logic to remember last email used for login

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -9,36 +9,55 @@ import { AuthService } from '../../services/auth/auth.service';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+  private loginRemember: boolean;
   public loginForm: FormGroup;
   public toggleErrorMsg: boolean;
   public errorMsg: string;
   public invalidEmailMsg: string;
   public invalidPWMsg: string;
   public title: string;
+  public email: string;
 
   constructor(
     private router: Router,
     private formBuilder: FormBuilder,
     private auth: AuthService
-    ) { }
+  ) {
+  }
 
   ngOnInit() {
+    this.checkRemember();
+
     this.loginForm = this.formBuilder.group({
-      email: new FormControl('', [Validators.required, Validators.email]),
-      password: new FormControl('', Validators.required)
+      email: new FormControl(this.email, [Validators.required, Validators.email]),
+      password: new FormControl('', Validators.required),
+      remember: new FormControl(this.loginRemember)
     });
 
     this.setupVariables();
   }
 
+  private checkRemember() {
+    this.loginRemember = localStorage.getItem('login-remember') ? true : false;
+
+    if (this.loginRemember) {
+      this.email = localStorage.getItem('login-email');
+    }
+  }
+
   private setupVariables() {
-    this.errorMsg = "Incorrect email/password";
-    this.invalidEmailMsg = "Not a valid email";
-    this.invalidPWMsg = "Password cannot be empty";
-    this.title = "Login";
+    this.errorMsg        = 'Incorrect email/password';
+    this.invalidEmailMsg = 'Not a valid email';
+    this.invalidPWMsg    = 'Password cannot be empty';
+    this.title           = 'Login';
   }
 
   public login(e) {
+    this.loginRemember = this.loginForm.controls['remember'].value;
+    if (this.loginRemember) {
+      localStorage.setItem('login-remember', `${this.loginRemember}`);
+      localStorage.setItem('login-email', this.loginForm.controls['email'].value);
+    }
     if (this.loginForm.valid) {
       if (this.auth.authLogin(this.loginForm.value.email, this.loginForm.value.password)) {
         this.router.navigateByUrl('/dashboard');
--- src/app/services/auth/auth.service.ts
+++ src/app/services/auth/auth.service.ts
@@ -2,56 +2,56 @@ import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Injectable({
-	providedIn: 'root'
+  providedIn: 'root'
 })
 export class AuthService {
-	public isLoggedIn: boolean;
+  public isLoggedIn: boolean;
 
-	constructor(
-		private router: Router
-		) {
-		this.isLoggedIn = false;
-	}
+  constructor(
+    private router: Router
+  ) {
+    this.isLoggedIn = false;
+  }
 
-	public authLogin(email, pw) : boolean {
-		if (email && pw) {
-			const account = localStorage.getItem('acount');
-			if (account) {
-				const tempAccount = JSON.parse(account);
+  public authLogin(email, pw): boolean {
+    if (email && pw) {
+      const account = localStorage.getItem('acount');
+      if (account) {
+        const tempAccount = JSON.parse(account);
 
-				if (email == tempAccount.email && pw == tempAccount.pw) {
-					localStorage.setItem("isLoggedIn", "true");
-					this.isLoggedIn = true;
-					return true;
-				}
-			}
+        if (email === tempAccount.email && pw === tempAccount.pw) {
+          localStorage.setItem('isLoggedIn', 'true');
+          this.isLoggedIn = true;
+          return true;
+        }
+      }
 
-		}
-		this.isLoggedIn = false;
-		return false;
-	}	
+    }
+    this.isLoggedIn = false;
+    return false;
+  }
 
-	public authSignup(email, pw) : boolean {
-		if (email && pw) {
-			const account = {email: email, pw: pw};
-			localStorage.setItem("isLoggedIn", "true");
-			localStorage.setItem("account", JSON.stringify(account));
-			this.isLoggedIn = true;
-			return true;
-		} else {
-			this.isLoggedIn = false;
-			return false;
-		}
-	}
+  public authSignup(email, pw): boolean {
+    if (email && pw) {
+      const account = {email: email, pw: pw};
+      localStorage.setItem('isLoggedIn', 'true');
+      localStorage.setItem('account', JSON.stringify(account));
+      this.isLoggedIn = true;
+      return true;
+    } else {
+      this.isLoggedIn = false;
+      return false;
+    }
+  }
 
-	public authLogout() : void {
-		localStorage.clear();
-		this.isLoggedIn = false;
-		this.router.navigateByUrl('/login');
-	}
+  public authLogout(): void {
+    localStorage.clear();
+    this.isLoggedIn = false;
+    this.router.navigateByUrl('/login');
+  }
 
-	public checkLoggedIn() : boolean {
-		this.isLoggedIn = localStorage.getItem("isLoggedIn") == "true";
-		return this.isLoggedIn;
-	}
+  public checkLoggedIn(): boolean {
+    this.isLoggedIn = localStorage.getItem('isLoggedIn') === 'true';
+    return this.isLoggedIn;
+  }
 }
